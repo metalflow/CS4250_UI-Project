84d27e36f5a9c9bca79e8f8234f9b2bb636aa2e9
Trevor Smith <trevor.smith@sendgrid.com>
2014-09-24 11:09:47 -0600

    Add text/img loading functionality

--- CS4250_UI-Project/src/cs4250_ui/project/CS4250_UIProject.java
+++ CS4250_UI-Project/src/cs4250_ui/project/CS4250_UIProject.java
@@ -1,51 +1,115 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs4250_ui.project;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-/**
- *
- * @author trevorsmith
- */
-public class CS4250_UIProject extends Application {
-    
+public final class CS4250_UIProject extends Application {
+
+    // This was created for testing purposes
+    private Desktop desktop = Desktop.getDesktop();
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(final Stage stage) {
+        stage.setTitle("File Chooser Sample");
+
+        final FileChooser fileChooser = new FileChooser();
+        final Button openButton = new Button("Open Image");
+        final Button openTextButton = new Button("Open Text");   
+
+        openButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    configureFileChooserImage(fileChooser);
+                    File file = fileChooser.showOpenDialog(stage);
+                    if (file != null) {
+                        openFile(file);
+                    }
+                }
+            });
+
+        openTextButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    configureFileChooserText(fileChooser);                               
+                    File file = fileChooser.showOpenDialog(stage);
+                    if (file != null) {
+                        openFile(file);
+                    }
+                }
+            });
+
+
+        final GridPane inputGridPane = new GridPane();
+
+        GridPane.setConstraints(openButton, 0, 1);
+        GridPane.setConstraints(openTextButton, 1, 1);
+        inputGridPane.setHgap(6);
+        inputGridPane.setVgap(6);
+        inputGridPane.getChildren().addAll(openButton, openTextButton);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        final Pane rootGroup = new VBox(12);
+        rootGroup.getChildren().addAll(inputGridPane);
+        rootGroup.setPadding(new Insets(12, 12, 12, 12));
+
+        stage.setScene(new Scene(rootGroup));
+        stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
+    }
+
+    private static void configureFileChooserImage(
+        final FileChooser fileChooser) {      
+            fileChooser.setTitle("View Pictures");
+            fileChooser.setInitialDirectory(
+                new File(System.getProperty("user.home"))
+            );                 
+            fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+            );
     }
     
+    private static void configureFileChooserText(
+        final FileChooser fileChooser) {      
+            fileChooser.setTitle("View Text Files");
+            fileChooser.setInitialDirectory(
+                new File(System.getProperty("user.home"))
+            );                 
+            fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text", "*.txt")
+            );
+    }
+    
+    private void openFile(File file) {
+        /*
+            Charles and Jaime, This is our test to get image and text loading working.  
+            This is called by the handlers of the FileChooser.
+        */ 
+       
+        try {
+            desktop.open(file);
+        } catch (IOException ex) {
+            Logger.getLogger(CS4250_UIProject.class.getName()).log(
+                Level.SEVERE, null, ex
+            );
+        }
+    }
+
 }
